9ae783d3028a98099183149f16b21014931f47aa
Huy Banh <huy.banh@jinnova.com>
2014-03-19 11:13:51 +0700

    updated to new api: NO DAO

--- src/main/java/com/jinnova/smartpad/common/sample/SampleDataGenerator.java
+++ src/main/java/com/jinnova/smartpad/common/sample/SampleDataGenerator.java
@@ -2,8 +2,6 @@ package com.jinnova.smartpad.common.sample;
 
 import java.sql.SQLException;
 
-import com.jinnova.smartpad.db.IBranchDao;
-import com.jinnova.smartpad.db.IUserDao;
 import com.jinnova.smartpad.partner.IBranch;
 import com.jinnova.smartpad.partner.IUser;
 import com.jinnova.smartpad.partner.IPartnerManager;
@@ -15,21 +13,19 @@ public class SampleDataGenerator {
 	public static void main(String[] args) throws SQLException {
 		SmartpadCommon.initialize();
 		IPartnerManager pm = SmartpadCommon.getPartnerManager();
-		IUserDao userDao = pm.getUserDao();
 		IUser u;
-		u = userDao.createPrimaryUser("lotte", "abc123");
+		u = pm.createPrimaryUser("lotte", "abc123");
 		u.setPasshash(PartnerUtils.md5("123abc"));
-		userDao.updateUser(u);
-		u = userDao.loadUser("lotte");
+		pm.updateUser(u);
+		u = pm.loadUser("lotte");
 		System.out.println(u.getLogin());
 		
-		u = userDao.createUser(u, "lotte2", "x");
+		u = pm.createUser(u, "lotte2", "x");
 		System.out.println(u.getLogin());
 		
-		IBranchDao bdao = pm.getBranchDao();
-		IBranch branch = bdao.loadBranch("lotte");
+		IBranch branch = pm.loadBranch("lotte");
 		branch.setName("Lotteria");
-		bdao.updateBranch(branch);
+		pm.updateBranch(branch);
 	}
 
 }
