54333bb606ab33d441d1527d760defed05c929e0
huybanh <huy.banh@jinnova.com>
2014-03-19 17:42:14 +0700

    operation api

--- src/main/java/com/jinnova/smartpad/common/sample/SampleDataGenerator.java
+++ src/main/java/com/jinnova/smartpad/common/sample/SampleDataGenerator.java
@@ -2,7 +2,7 @@ package com.jinnova.smartpad.common.sample;
 
 import java.sql.SQLException;
 
-import com.jinnova.smartpad.partner.IBranch;
+import com.jinnova.smartpad.partner.IOperation;
 import com.jinnova.smartpad.partner.IUser;
 import com.jinnova.smartpad.partner.IPartnerManager;
 import com.jinnova.smartpad.partner.SmartpadCommon;
@@ -10,7 +10,11 @@ import com.jinnova.smartpad.partner.SmartpadCommon;
 public class SampleDataGenerator {
 
 	public static void main(String[] args) throws SQLException {
+		
+		//user
 		SmartpadCommon.initialize();
+		SmartpadCommon.getPartnerManager().clearDatabaseForTests();
+		
 		IPartnerManager pm = SmartpadCommon.getPartnerManager();
 		IUser primaryUser;
 		primaryUser = pm.createPrimaryUser("lotte", "abc123");
@@ -18,12 +22,16 @@ public class SampleDataGenerator {
 		pm.updateUser(primaryUser, primaryUser);
 		primaryUser = pm.login("lotte", "123abc");
 		System.out.println(primaryUser.getLogin());
-		IBranch branch = primaryUser.loadBranch();
+		
+		//branch
+		IOperation branch = primaryUser.loadBranch();
 		branch.setName("Lotteria");
 		primaryUser.updateBranch();
+		branch = primaryUser.loadBranch();
 		System.out.println(branch.getName());
 		
-		IUser u = pm.createUser(primaryUser, "lotte2", "x");
+		pm.createUser(primaryUser, "lotte2", "x");
+		IUser u = pm.login("lotte2", "x");
 		System.out.println(u.getLogin());
 		
 		branch = u.loadBranch();
